2f681a3b95af19c2ff58afc23ff7068e25022139
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-22 15:42:51 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'Mitch'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer= 'Linda'
+}
